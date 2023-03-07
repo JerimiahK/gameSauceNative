cddee6f60e68a7d26b6e5c3c8ee279a8e74118df
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-07 15:47:57 -0700

    refactored homepage and navtabs so that they are React Native compliant

--- gameSauceNative/components/NavTabs.js
+++ gameSauceNative/components/NavTabs.js
@@ -1,10 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
-import { Typography } from "@mui/material";
-import Button from "@mui/material/Button";
-import HomeIcon from "@mui/icons-material/Home";
-import SportsHockeyIcon from "@mui/icons-material/SportsHockey";
-import LoginIcon from "@mui/icons-material/Login";
-import LogoutIcon from "@mui/icons-material/Logout";
+import { View, Text, StyleSheet, Button } from "react-native";
 // import Auth from "../utils/auth";
 
 export default function NavTabs() {
@@ -14,199 +8,170 @@ export default function NavTabs() {
   // };
   
 return (
-        
-        <View style={style.navContainer}>
-          <View>
-          <Text style={style.navbarText}>
-          GameSauce
-          </Text>
-          </View>
-          <View style={style.headerButtons}>
-            <Button sx={{ minWidth: "0px" }}>
-              <HomeIcon sx={{ color: "#92b9e0" }} />
-              <Typography
-                sx={{
-                  color: "#C5C6C7",
-                }}
-              >
-                <Text
-                >
-                  Home
-                </Text>
-              </Typography>
-            </Button>
-            <Button sx={{ minWidth: "0px" }}>
-              <SportsHockeyIcon sx={{ color: "#92b9e0" }} />
-              <Typography
-                sx={{
-                  color: "#C5C6C7",
-                }}
-              >
-                <Text
-                >
-                  Games
-                </Text>
-              </Typography>
-            </Button>
-            <Button sx={{ minWidth: "0px" }}>
-              <LoginIcon sx={{ color: "#92b9e0" }} />
-              <Typography
-                sx={{
-                  color: "#C5C6C7",
-                }}
-              >
-                <Text>
-                  Login
-                </Text>
-              </Typography>
-            </Button>
-          </View>
-        </View>
-  
-    // <View style="navContainer">
-    //   <View style="headerText">
-    //     <a id="navbarText" href="/">
-    //       GameSauce
-    //     </a>
-    //   </View>
-    //   {Auth.loggedIn() === true ? (
-    //     <>
-    //       <View style="headerButtons">
-    //         <Button sx={{ minWidth: "0px" }}>
-    //           <HomeIcon sx={{ color: "#92b9e0" }} />
-    //           <Typography
-    //             sx={{
-    //               color: "#C5C6C7",
-    //             }}
-    //           >
-    //             <Text
-    //               sx={{
-    //                 textDecoration: "none",
-    //               }}
-    //               color="inherit"
-    //               href="/"
-    //             >
-    //               Home
-    //             </Text>
-    //           </Typography>
-    //         </Button>
-    //         <Button sx={{ minWidth: "0px" }}>
-    //           <SportsHockeyIcon sx={{ color: "#92b9e0" }} />
-    //           <Typography
-    //             sx={{
-    //               color: "#C5C6C7",
-    //             }}
-    //           >
-    //             <Text
-    //               sx={{
-    //                 textDecoration: "none",
-    //               }}
-    //               color="inherit"
-    //               href="/games"
-    //             >
-    //               Games
-    //             </Text>
-    //           </Typography>
-    //         </Button>
-    //         <Button sx={{ minWidth: "0px" }}>
-    //           <LogoutIcon onClick={logout} sx={{ color: "#92b9e0" }} />
-    //           <Typography
-    //             sx={{
-    //               color: "#C5C6C7",
-    //             }}
-    //           >
-    //             <Text
-    //               sx={{
-    //                 textDecoration: "none",
-    //               }}
-    //               color="inherit"
-    //               onClick={logout}
-    //             >
-    //               Logout
-    //             </Text>
-    //           </Typography>
-    //         </Button>
-    //       </View>
-    //     </>
-    //   ) : (
-    //     <>
-    //       <View style="headerButtons">
-    //         <Button sx={{ minWidth: "0px" }}>
-    //           <HomeIcon sx={{ color: "#92b9e0" }} />
-    //           <Typography
-    //             sx={{
-    //               color: "#C5C6C7",
-    //             }}
-    //           >
-    //             <Text
-    //               sx={{
-    //                 textDecoration: "none",
-    //               }}
-    //               color="inherit"
-    //               href="/"
-    //             >
-    //               Home
-    //             </Text>
-    //           </Typography>
-    //         </Button>
-    //         <Button sx={{ minWidth: "0px" }}>
-    //           <SportsHockeyIcon sx={{ color: "#92b9e0" }} />
-    //           <Typography
-    //             sx={{
-    //               color: "#C5C6C7",
-    //             }}
-    //           >
-    //             <Text
-    //               sx={{
-    //                 textDecoration: "none",
-    //               }}
-    //               color="inherit"
-    //               href="/games"
-    //             >
-    //               Games
-    //             </Text>
-    //           </Typography>
-    //         </Button>
-    //         <Button sx={{ minWidth: "0px" }}>
-    //           <LoginIcon sx={{ color: "#92b9e0" }} />
-    //           <Typography
-    //             sx={{
-    //               color: "#C5C6C7",
-    //             }}
-    //           >
-    //             <Text
-    //               sx={{
-    //                 textDecoration: "none",
-    //               }}
-    //               color="inherit"
-    //               href="/login"
-    //             >
-    //               Login
-    //             </Text>
-    //           </Typography>
-    //         </Button>
-    //       </View>
-    //     </>
-    //   )}
-    // </View>
-  );
+  <View style={style.navContainer}>
+    <View>
+      <Text style={style.navbarText}>GameSauce</Text>
+    </View>
+    <View style={style.headerButtons}>
+      <Button
+      title="Home"
+      >
+        <Text>Home</Text>
+      </Button>
+      <Button title="Games">
+          <Text>Games</Text>
+      </Button>
+      <Button title="Login">
+          <Text>Login</Text>
+      </Button>
+    </View>
+  </View>
+
+  // <View style="navContainer">
+  //   <View style="headerText">
+  //     <a id="navbarText" href="/">
+  //       GameSauce
+  //     </a>
+  //   </View>
+  //   {Auth.loggedIn() === true ? (
+  //     <>
+  //       <View style="headerButtons">
+  //         <Button sx={{ minWidth: "0px" }}>
+  //           <HomeIcon sx={{ color: "#92b9e0" }} />
+  //           <Typography
+  //             sx={{
+  //               color: "#C5C6C7",
+  //             }}
+  //           >
+  //             <Text
+  //               sx={{
+  //                 textDecoration: "none",
+  //               }}
+  //               color="inherit"
+  //               href="/"
+  //             >
+  //               Home
+  //             </Text>
+  //           </Typography>
+  //         </Button>
+  //         <Button sx={{ minWidth: "0px" }}>
+  //           <SportsHockeyIcon sx={{ color: "#92b9e0" }} />
+  //           <Typography
+  //             sx={{
+  //               color: "#C5C6C7",
+  //             }}
+  //           >
+  //             <Text
+  //               sx={{
+  //                 textDecoration: "none",
+  //               }}
+  //               color="inherit"
+  //               href="/games"
+  //             >
+  //               Games
+  //             </Text>
+  //           </Typography>
+  //         </Button>
+  //         <Button sx={{ minWidth: "0px" }}>
+  //           <LogoutIcon onClick={logout} sx={{ color: "#92b9e0" }} />
+  //           <Typography
+  //             sx={{
+  //               color: "#C5C6C7",
+  //             }}
+  //           >
+  //             <Text
+  //               sx={{
+  //                 textDecoration: "none",
+  //               }}
+  //               color="inherit"
+  //               onClick={logout}
+  //             >
+  //               Logout
+  //             </Text>
+  //           </Typography>
+  //         </Button>
+  //       </View>
+  //     </>
+  //   ) : (
+  //     <>
+  //       <View style="headerButtons">
+  //         <Button sx={{ minWidth: "0px" }}>
+  //           <HomeIcon sx={{ color: "#92b9e0" }} />
+  //           <Typography
+  //             sx={{
+  //               color: "#C5C6C7",
+  //             }}
+  //           >
+  //             <Text
+  //               sx={{
+  //                 textDecoration: "none",
+  //               }}
+  //               color="inherit"
+  //               href="/"
+  //             >
+  //               Home
+  //             </Text>
+  //           </Typography>
+  //         </Button>
+  //         <Button sx={{ minWidth: "0px" }}>
+  //           <SportsHockeyIcon sx={{ color: "#92b9e0" }} />
+  //           <Typography
+  //             sx={{
+  //               color: "#C5C6C7",
+  //             }}
+  //           >
+  //             <Text
+  //               sx={{
+  //                 textDecoration: "none",
+  //               }}
+  //               color="inherit"
+  //               href="/games"
+  //             >
+  //               Games
+  //             </Text>
+  //           </Typography>
+  //         </Button>
+  //         <Button sx={{ minWidth: "0px" }}>
+  //           <LoginIcon sx={{ color: "#92b9e0" }} />
+  //           <Typography
+  //             sx={{
+  //               color: "#C5C6C7",
+  //             }}
+  //           >
+  //             <Text
+  //               sx={{
+  //                 textDecoration: "none",
+  //               }}
+  //               color="inherit"
+  //               href="/login"
+  //             >
+  //               Login
+  //             </Text>
+  //           </Typography>
+  //         </Button>
+  //       </View>
+  //     </>
+  //   )}
+  // </View>
+);
 }
 
 const style = StyleSheet.create({
   navContainer: {
     display: "flex",
+    flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
   navbarText: {
-    paddingLeft: "10px",
+    paddingLeft: "5%",
     color: "#92b9e0",
-    fontFamily: "'Montserrat', sans-serif",
-    fontSize: "250%",
+    fontSize: "40%"
   },
   headerButtons: {
     display: "flex",
     flexDirection: "column",
     alignItems: "flex-start",
-  }
-})
+    margin: "5%",
+  },
+});
--- gameSauceNative/components/PageContainer.js
+++ gameSauceNative/components/PageContainer.js
@@ -12,14 +12,13 @@ const Stack = createNativeStackNavigator();
 const style = StyleSheet.create({
   page: {
     fontFamily: "'Rubik', sans-serif",
-    padding: "10px",
+    paddingTop: "5%",
     backgroundColor: "#282828",
     width: "100%",
     height: "100%",
   },
   currentGame: {
     height: "100%",
-    marginTop: "20%",
     backgroundColor: "#282828",
   },
 });
