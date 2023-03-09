49d01424eaf76c71525593b8a91c1a0413a94965
Jerimiah Kripakov <115738969+JerimiahK@users.noreply.github.com>
2023-03-08 20:06:34 -0700

    added a games button to the bottom of the page

--- gameSauceNative/components/PageContainer.js
+++ gameSauceNative/components/PageContainer.js
@@ -1,5 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useNavigation } from "@react-navigation/native";
 import { StyleSheet, View, Pressable } from "react-native";
+import Icon from "react-native-vector-icons/MaterialIcons";
 import Homepage from "./pages/Homepage";
 import Games from "./pages/Games";
 // import SelectedGame from "./pages/SelectedGame";
@@ -9,6 +11,7 @@ import Games from "./pages/Games";
 const Stack = createNativeStackNavigator();
 
 export default function PageContainer() {
+  const navigation = useNavigation();
   return (
     <View style={style.page}>
       <View style={style.currentGame}>
@@ -22,6 +25,16 @@ export default function PageContainer() {
           <Stack.Screen name="Homepage" component={Homepage} />
           <Stack.Screen name="Games" component={Games} />
         </Stack.Navigator>
+        <View style={style.navContainer}>
+          <View style={style.navbar}>
+            <Icon.Button
+              style={style.icon}
+              name="games"
+              size={50}
+              onPress={() => navigation.navigate("Games")}
+            >Games</Icon.Button>
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -30,12 +43,29 @@ export default function PageContainer() {
 const style = StyleSheet.create({
   page: {
     paddingTop: "5%",
-    backgroundColor: "#3a0145",
+    backgroundColor: "#43014f",
     width: "100%",
     height: "100%",
   },
   currentGame: {
     height: "100%",
     backgroundColor: "#fff",
+  },
+  navContainer: {
+    position: "absolute",
+    alignItems: "center",
+    bottom: 20,
+    width: "100%",
+  },
+  navbar: {
+    flexDirection: "row",
+    backgroundColor: "#00d4ff",
+    justifyContent: "center",
+    alignContent: "center",
+    borderRadius: 40,
+  },
+  icon: {
+    width: "100%",
+    backgroundColor: "#43014f",
   },
 });
